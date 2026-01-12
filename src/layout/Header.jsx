e10cc2e35f06ccfc0bbc55be3a6f9d2ae6972c5f
DARTITH002 <chandarith002@gmail.com>
2026-01-12 16:25:45 +0700

    udpate menu

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { FcAbout } from "react-icons/fc";
+import { HiOutlineHome } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <nav className="shadow-md">
+        <div className="w-full md:w-4/5  m-auto flex justify-between">
+          <div className="w-16">
+            <img
+              className="w-full h-full"
+              src="https://etec-system.42web.io/assets/etec.png"
+              alt=""
+            />
+          </div>
 
-export default Header
+          <div className="h-16">
+            <ul className="flex gap-3.5 items-center h-full ">
+              <li>
+                <Link
+                  to={"/"}
+                  className="flex gap-1.5 items-center text-lg font-medium"
+                >
+                  <HiOutlineHome className="text-xl" />
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/about"}
+                  className="flex gap-1.5 items-center text-lg font-medium"
+                >
+                  <FcAbout className="text-xl" />
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
